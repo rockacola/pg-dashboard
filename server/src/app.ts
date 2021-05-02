6cf96fc4cd6289120f0c7c0a3d3a9b2409f92dd9
Travis <travis@localhost.dev>
2021-05-03 03:14:14 +1000

    Add nodemon and dotenv usage

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ import { ExecRoutes } from './exec/exec.routes.config'
 
 const app: express.Application = express()
 const server: http.Server = http.createServer(app)
-const port = 3000 // TODO: move to config
+const port = process.env.SERVER_PORT
 const routes: Array<CommonRoutesConfig> = []
 const debugLog: debug.IDebugger = debug('app')
 
