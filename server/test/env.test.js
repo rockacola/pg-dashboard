592608696c4a2cded4385fa3fbe924c783f07c64
Travis <146148+rockacola@users.noreply.github.com>
2021-05-06 01:13:26 +1000

    Tweak dotenv test

--- server/test/env.test.js
+++ server/test/env.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: './.env.test' })
 
 describe('.env.test', () => {
-  test('API_BASE_URL', () => {
-    expect(process.env.API_BASE_URL).toBe('http://localhost:3300/api')
+  test('NODE_ENV', () => {
+    expect(process.env.NODE_ENV).toBe('test')
   })
 })
